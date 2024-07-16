5ba8ff1c47d6461a7a816c49941d702a231800ae
Watunder <vinschen66@gmail.com>
2024-07-17 06:42:38 +0800

    Update rollup.config.js

--- components/coherent-gameface-components/rollup.config.js
+++ components/coherent-gameface-components/rollup.config.js
@@ -1,6 +1,30 @@
+import url from 'url';
+import path from 'path';
+import { globSync } from 'glob';
+import html from '@rollup/plugin-html';
+import inline from 'rollup-plugin-inline-string';
+import resolve from '@rollup/plugin-node-resolve';
 
 export default [
     {
-
+        input: Object.fromEntries(
+            globSync('./*/script.js').map((file) => [
+                path.dirname(file),
+                file
+            ])
+        ),
+        plugins: [
+            resolve(),
+            inline({
+                extensions: ['.html'],
+                minify: true
+            })
+        ],
+        output: {
+            format: 'esm',
+            dir: './_out',
+            chunkFileNames: '[name].js'
+        },
+        context: "globalThis"
     }
 ];
