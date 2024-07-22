c306329ffbdac08d932f07355479cc3018a00b1c
Watunder <vinschen66@gmail.com>
2024-07-22 14:09:54 +0800

    Add `interaction-manager` library

---
+++ components/coherent-gameface-components/_lib/index.js
@@ -0,0 +1,2 @@
+export * from "./coherent-gameface-components/components.js";
+export * from "./interaction-manager/src/index.js";
---
+++ components/coherent-gameface-components/_lib/interaction-manager/rollup.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        input: './src/index.js',
+        output: {
+            format: 'esm',
+            file: './dist/interaction-manager.js'
+        }
+    }
+];
