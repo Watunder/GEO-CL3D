ca4c7962e92aaa1399a412c12493533594eb7852
Watunder <vinschen66@gmail.com>
2024-07-22 14:06:36 +0800

    Update `coherent-gameface-components` packages

--- components/coherent-gameface-components/rollup.config.js
+++ components/coherent-gameface-components/rollup.config.js
@@ -5,7 +5,50 @@ import html from '@rollup/plugin-html';
 import inline from 'rollup-plugin-inline-string';
 import resolve from '@rollup/plugin-node-resolve';
 
+// rollup plugin
+function module2Path(option) {
+    const { includes } = option;
+    Object.keys(includes).forEach((targetName) => {
+        includes[targetName] = includes[targetName]
+            .split(path.sep)
+            .join(path.posix.sep);
+    });
+    return {
+        name: "module2Path",
+        resolveId: {
+            order: "pre",
+            async handler(source, importer, options) {
+                if (source && Object.keys(includes).includes(source)) {
+                    const t = includes[source];
+                    if (t) {
+                        let resolution = await this.resolve(t, undefined, {
+                            skipSelf: true,
+                            ...options,
+                        });
+                        if (!resolution) {
+                            resolution = {
+                                id: t,
+                                moduleSideEffects: true,
+                                external: true,
+                            };
+                        }
+                        return resolution;
+                    }
+                }
+                return null;
+            },
+        },
+    };
+}
+
 export default [
+    {
+        input: './_lib/index.js',
+        output: {
+            format: 'esm',
+            file: './_out/gameface.js',
+        }
+    },
     {
         input: Object.fromEntries(
             globSync('./*/script.js').map((file) => [
@@ -14,7 +57,14 @@ export default [
             ])
         ),
         plugins: [
-            resolve(),
+            module2Path({
+                includes: {
+                    'coherent-gameface-components': '../gameface.js'
+                }
+            }),
+            resolve({
+                
+            }),
             inline({
                 extensions: ['.html'],
                 minify: true
@@ -22,7 +72,7 @@ export default [
         ],
         output: {
             format: 'esm',
-            dir: './_out',
+            dir: './_out/gameface/',
             chunkFileNames: '[name].js'
         },
         context: "globalThis"
