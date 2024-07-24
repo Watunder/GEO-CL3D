37956c6364e8361a24d067b57312db534f9832dc
Watunder <vinschen66@gmail.com>
2024-07-24 21:08:30 +0800

    Update rollup.config.js

--- components/coherent-gameface-components/rollup.config.js
+++ components/coherent-gameface-components/rollup.config.js
@@ -1,9 +1,10 @@
 import url from 'url';
 import path from 'path';
 import { globSync } from 'glob';
-import html from '@rollup/plugin-html';
+import multi from '@rollup/plugin-multi-entry';
 import inline from 'rollup-plugin-inline-string';
 import resolve from '@rollup/plugin-node-resolve';
+import inputCSS from 'rollup-plugin-input-with-css';
 
 // rollup plugin
 function module2Path(option) {
@@ -76,5 +77,44 @@ export default [
             chunkFileNames: '[name].js'
         },
         context: "globalThis"
+    },
+    {
+        input: Object.fromEntries(
+            globSync('./**/*.css', { ignore: './**/node_modules/**' }).map((file, index) => {
+                let name = `css-${file.slice(0, file.indexOf('\\'))}-${path.basename(file, '.css')}-${index}`
+                if (path.basename(file) == 'coherent-gameface-components-theme.css')
+                    name = 'css-coherent-gameface-components-theme';
+                return [
+                    name,
+                    file
+                ]
+            })
+        ),
+        plugins: [
+            inputCSS(),
+            inline({
+                extensions: ['.css'],
+                minify: true
+            })
+        ],
+        output: {
+            dir: './_out/gameface-style/'
+        }
+    },
+    {
+        input: [
+			...globSync('./_out/*/*.js', { ignore: './_out/gameface-demo/**' }).map((file) => {
+				return file;
+			})
+		],
+        plugins: [
+            multi({
+                entryFileName: 'gameface-bundle.js'
+            })
+        ],
+		output: {
+			format: 'esm',
+			dir: './_out/'
+		}
     }
 ];
